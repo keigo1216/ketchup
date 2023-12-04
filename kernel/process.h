94f3e9d9ae24a6f5608e0d5368433f8b085c3f4e
keigo1216 <sagankeigo17@gmail.com>
2023-12-04 14:40:40 +0900

    implement schedule algorithm

--- kernel/process.h
+++ kernel/process.h
@@ -11,9 +11,15 @@ struct process {
     uint8_t stack[8192];    // カーネルスタック
 };
 
+void yeild(void);
+
 __attribute__((naked))
 void switch_context (uint64_t *prev_sp, uint64_t *next_sp);
 
 struct process *create_process(uint64_t pc);
+
+extern struct process procs[PROCS_MAX];
+extern struct process *current_proc;
+extern struct process *idle_proc;
 
 #endif // __PROCESS_H__
