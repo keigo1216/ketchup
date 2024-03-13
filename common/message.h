5a4cc470b8cba7058272ec16136181c71192234e
shibatakeigo <sagankeigo17@gmail.com>
2024-03-14 00:43:22 +0900

    Add wait_for member to process struct

--- common/message.h
+++ common/message.h
@@ -1,9 +1,9 @@
 #include "common.h"
 
-typedef struct {
+struct message{
     int type; /*type of message*/
     int src;  /*source of process*/
     union {
         uint8_t data[0]; /*head pointer of message data*/
     };
-} message;
+};
